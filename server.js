8030dd8c911dedf7481a696847b31886b02be135
Ben Halverson <benhalverson33@gmail.com>
2017-01-16 19:06:42 -0800

    put method works

--- server.js
+++ server.js
@@ -49,6 +49,38 @@ app.get('/cart/list', (req, res) => {
     }
   });
 });
+
+//get one document by id
+app.get('/cart/list/:product_id', (req, res) => {
+  Product.findById(req.params.product_id, (err, product)=> {
+    if(err) {
+      console.error(`Can not find the id ${req.params.product_id}, ${err}`);
+    } else {
+      res.json(product);
+    }
+  });
+});
+
+app.put('/cart/update/:product_id', (req, res) => {
+  Product.findById(req.params.product_id, (err, product)=> {
+    if(err) {
+      console.error(`Can not find the id ${req.params.product_id}, ${err}`);
+    } else {
+      product.name = req.body.name;
+      product.description = req.body.description;
+      product.price = req.body.price;
+      product.imageurl = req.body.imageurl;
+      product.save((err) => {
+        if(err) {
+          res.send({error: err});
+        } else {
+          res.json({message: 'Product updated'});
+        }
+      })
+
+    }
+  });
+});
 //fix this later...
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
